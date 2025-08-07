755f31c219378cf0d93cf6109951282b796a7e00
fooberticus <fooberticus@protonmail.com>
2025-08-07 13:04:19 -0400

    fixed time formatting

--- src/main/java/com/fooberticus/steamtools/models/server/ServerPlayer.java
+++ src/main/java/com/fooberticus/steamtools/models/server/ServerPlayer.java
@@ -6,4 +6,15 @@ import lombok.Data;
 public class ServerPlayer {
     Long steam64Id;
     String timeOnServer;
+
+    public String getTimeOnServer() {
+        if (timeOnServer != null) {
+            if (timeOnServer.length() == 5) {
+                timeOnServer = "00:" + timeOnServer;
+            } else if (timeOnServer.length() == 7) {
+                timeOnServer = "0" + timeOnServer;
+            }
+        }
+        return timeOnServer;
+    }
 }
